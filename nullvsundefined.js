a59ae85ff9380c4d86ce070eb2621ae8c0174510
lucifer2340v <ksbnimil@gmail.com>
2020-03-14 10:26:03 +0600

    null-undefined

---
+++ nullvsundefined.js
@@ -0,0 +1,15 @@
+let pakhi;
+function add(n1,n2){
+console.log(n1+n2);
+return
+}
+function add(n1,n2){
+console.log(n1,n2);
+}
+const premik={name:"smart",phone:4545};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+
+let ages=[2,5,9];
+console.log(ages[11]);
